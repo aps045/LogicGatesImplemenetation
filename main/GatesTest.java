26ab29a96cb6177a8da191914547706afa23f3da
aps045 <aditya.singh1_cs18@gla.ac.in>
2019-10-23 20:22:49 +0530

    Execution Class

---
+++ main/GatesTest.java
@@ -0,0 +1,28 @@
+package main;
+
+import gates.NOTGate;
+import gates.ORGate;
+import gates.ANDGate;
+import gates.NANDGate;
+import gates.NORGate;
+import gates.XORGate;
+import gates.XNORGate;
+
+public class GatesTest {
+    public static void main(String[] args) {
+        ANDGate andGate = new ANDGate();
+        andGate.printTruthTable();
+        ORGate orGate = new ORGate();
+        orGate.printTruthTable();
+        NOTGate notGate = new NOTGate();
+        notGate.printTruthTable();
+        NANDGate nandGate = new NANDGate();
+        nandGate.printTruthTable();
+        NORGate norGate = new NORGate();
+        norGate.printTruthTable();
+        XORGate xorGate = new XORGate();
+        xorGate.printTruthTable();
+        XNORGate xnorGate = new XNORGate();
+        xnorGate.printTruthTable();
+    }
+}
